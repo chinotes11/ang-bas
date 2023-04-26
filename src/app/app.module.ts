58a7f216c7e2d555fd2fe60b77f8bb0c1bfbbd99
gerardo.mauriesr <gerardo.mauriesr@gmail.com>
2023-04-26 13:29:40 -0600

    Fin de sección

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeroComponent } from './heroes/hero/hero.component';
+import { ListComponent } from './heroes/list/list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroComponent,
+    ListComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
